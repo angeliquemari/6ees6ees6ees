38434de18d88fb99fc53d684759fb5a178633371
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-11 09:00:37 -0700

    add grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,10 @@
 class Grub {
+    constructor(age = 0, color = 'pink') {
+        this.age = age;
+        this.color = color;
+        this.food = 'jelly';
+    }
+    eat() {
+        return 'Mmmmmmmmm jelly';
+    }
 };
